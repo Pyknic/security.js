4a9105cd9c5d1456a1c90e5de5f73dde560c2101
pyknic <emil@speedment.com>
2017-02-17 13:41:09 -0800

    Build using closure compiler

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,19 @@
 module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
-        pkg : grunt.file.readJSON('package.json'),
-        copy : {
-            build : {
-                cwd : 'app',
-                src : ['src/**'],
-                dest : 'dest',
-                expand : true
-            }
-        },
-        clean : {
-            build : {
-                src : 'dest'
+        'closure-compiler': {
+            security: {
+                closurePath  : '/home/pyknic/closure_compiler',
+                js           : 'src/security.js',
+                jsOutputFile : 'bin/security.min.js',
+                maxBuffer    : 500,
+                options      : {
+                    compilation_level : 'ADVANCED_OPTIMIZATIONS',
+                    language_in       : 'ECMASCRIPT5_STRICT'
+                }
             }
         }
     });
     
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.registerTask(
-        'build',
-        'Compiles all the assets and copies the files to the build directory.',
-       [ 'clean', 'copy' ]
-    );
+    grunt.loadNpmTasks('grunt-closure-compiler');
 };
